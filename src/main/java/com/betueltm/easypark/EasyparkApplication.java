3882edb26c8eaa18629577fc099b0469807bfcc8
betuel.gandolfi <betuelgandolfi@NOTE53237>
2024-12-19 13:36:40 -0300

    first commit

---
+++ src/main/java/com/betueltm/easypark/EasyparkApplication.java
@@ -0,0 +1,13 @@
+package com.betueltm.easypark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyparkApplication.class, args);
+	}
+
+}
